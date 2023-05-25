2d23ab691abe7543f46bdb2bbccd86d25362ddc3
Jonathan Herrera <msjona28@outlook.com>
2023-05-25 06:58:57 -0700

    Aplicación sencilla con métodos para consultar entidades, así como para agregar relaciones (propietarios y arrendatarios)

--- app.js
+++ app.js
@@ -1,29 +1,95 @@
 const models = require('./models');
 
 const consultaPropiedades = async () => {
-    r = await models.Propiedad.findAll();
-    await r.forEach(p => {
-        console.log(p.dataValues);
+    propiedades = await models.Propiedad.findAll();
+    await propiedades.forEach(propiedad => {
+        console.log(propiedad.dataValues);
     });
     models.sequelize.close();
 }
 
-const agregaPropietarios = async () => {
-    const prop = await models.Propiedad.findByPk(1);
-    //console.log(prop.dataValues);
-    const per1 = await models.Persona.findByPk(2);
-    const per2 = await models.Persona.findByPk(1);
-    //console.log(per.dataValues)
-    await prop.addPropietarios([per1,per2]);
+const consultaPersonas = async () => {
+    personas = await models.Persona.findAll();
+    await personas.forEach(persona => {
+        console.log(persona.dataValues);
+    });
+    models.sequelize.close();
+}
+
+const consultaPropietarios = async () => {
+    propietarios = await models.Propietario.findAll();
+    await propietarios.forEach(propietario => {
+        console.log(propietario.dataValues);
+    });
+    models.sequelize.close();
+}
+
+const consultaArrendatarios = async () => {
+    arrendatarios = await models.Arrendatario.findAll();
+    await arrendatarios.forEach(arrendatario => {
+        console.log(arrendatario.dataValues);
+    });
+    models.sequelize.close();
+}
+
+const agregaPropietarios = async (idPropiedad, idsPropietarios) => {
+    const prop = await models.Propiedad.findByPk(idPropiedad);
+    console.log(prop.dataValues);
+
+    const personas = await models.Persona.findAll({
+        where: {
+            id: idsPropietarios
+        }
+    });
+    personas.forEach(p => {
+        console.log(p.dataValues);
+    });
+
+    await prop.addPropietarios(personas);
     const propietarios = await prop.getPropietarios();
     await propietarios.forEach(p => {
         console.log(p.nombre);
-        
     });
-
-
 
     models.sequelize.close();
 }
 
-agregaPropietarios();
+const agregaArrendatarios = async (idPropiedad, idsArrendatarios) => {
+    const prop = await models.Propiedad.findByPk(idPropiedad);
+    console.log(prop.dataValues);
+
+    const personas = await models.Persona.findAll({
+        where: {
+            id: idsArrendatarios
+        }
+    });
+    personas.forEach(p => {
+        console.log(p.dataValues);
+    });
+
+    await prop.addArrendatarios(personas);
+    const arrendatarios = await prop.getArrendatarios();
+    await arrendatarios.forEach(p => {
+        console.log(p.nombre);
+    });
+
+    models.sequelize.close();
+}
+
+// console.log("Personas:");
+// consultaPersonas();
+
+// console.log("Propiedades");
+// consultaPropiedades();
+
+// agregaPropietarios(1, 1);
+// agregaPropietarios(2, [1, 2]);
+// agregaPropietarios(3, [4, 5]);
+
+// consultaPropietarios();
+
+// agregaArrendatarios(1, 3);
+// agregaArrendatarios(2, [5, 6]);
+// agregaArrendatarios(3, 1);
+
+// consultaArrendatarios();
